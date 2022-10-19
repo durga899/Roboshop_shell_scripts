7a98d2aeae4bfc5a297961077427a2a2addb188d
durga <durgarajesh899@gmail.com>
2022-10-19 21:46:57 +0530

    Adding if in Exit status

--- Frontend.sh
+++ Frontend.sh
@@ -1,10 +1,10 @@
  echo Installing Nginx
  yum install nginx -y &>>/tmp/frontend
  status = $?
- if [status -eq 0] then
-   echo "Nginx installed successfully"
+ if [ status -eq 0] then
+   echo Nginx installed successfully
  else
-   echo "Installation failed"
+   echo Installation failed
  fi
 
  systemctl enable nginx
