b5f698593d8ce89c13d9ab18f5d703b6615961e7
durga <durgarajesh899@gmail.com>
2022-10-21 18:33:29 +0530

    SED in Mongodb

--- Mongodb.sh
+++ Mongodb.sh
@@ -12,7 +12,7 @@ systemctl enable mongod
 systemctl start mongod
 
 echo Editing mongo file
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo status = $?
 
 echo Downloading mongodb schema
