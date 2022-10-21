06e9977143775989632ce865c7cc1b26246ce8aa
durga <durgarajesh899@gmail.com>
2022-10-21 18:37:07 +0530

    SED in Mongodb

--- Mongodb.sh
+++ Mongodb.sh
@@ -15,6 +15,8 @@ echo Editing mongo file
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo status = $?
 
+systemctl start mongod
+
 echo Downloading mongodb schema
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>LOG_FILE
 echo status = $?
