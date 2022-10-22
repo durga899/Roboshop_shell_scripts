ae9b86295ed1bb017594471c5ec7c4ea6beb532d
durga <durgarajesh899@gmail.com>
2022-10-22 07:36:32 +0530

    Adding function in catalogue service

--- Mongodb.sh
+++ Mongodb.sh
@@ -1,40 +1,56 @@
 LOG_FILE=/tmp/mongodb
+
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+  echo Please execute script in Root user or with Sudo Priviliges
+  exit 1
+fi
+
+StatusCheck(){
+  if [ $1 -eq 0 ]; then
+    echo -e Status = "\e[32msuccess\e[0m"
+  else
+    echo -e status = "\e31mfailure\e[0m"
+    exit 1
+  fi
+}
+
 echo Getting Mongodb repo
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 echo Installing mongodb
 yum install -y mongodb-org &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 
 echo Editing mongo file
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo status = $?
+StatusCheck $?
 
 echo Starting mongodb
 systemctl enable mongod &>>LOG_FILE
 systemctl restart mongod &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 echo Downloading mongodb schema
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"  &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 cd /tmp
 echo Unzipping mongodb
 unzip mongodb.zip &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 cd mongodb-main
 
 echo Load Catalouge service
 mongo < catalogue.js  &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 echo Load User service schema
 mongo < users.js  &>>LOG_FILE
-echo status = $?
+StatusCheck $?
 
 
 
