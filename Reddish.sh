1c164968497f1d5c592436a9af6d068786cb1dac
durga <durgarajesh899@gmail.com>
2022-10-23 11:52:35 +0530

    Adding Redis

---
+++ Reddish.sh
@@ -0,0 +1,31 @@
+LOG_FILE=/tmp/redis
+
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+  echo Please execute script in Root user or with Sudo Priviliges
+  exit 1
+fi
+
+source common.sh
+
+echo Getting Repos
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>LOG_FILE
+StatusCheck $?
+
+echo Enabling module
+dnf module enable redis:remi-6.2 -y &>>LOG_FILE
+StatusCheck $?
+
+echo Installing redis
+yum install redis -y &>>LOG_FILE
+StatusCheck $?
+
+echo Updating Redis listen addrs
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf &>>LOG_FILE
+StatusCheck $?
+
+systemctl enable redis &>>LOG_FILE
+echo Restarting redis
+systemctl start redis &>>LOG_FILE
+StatusCheck $?
+
