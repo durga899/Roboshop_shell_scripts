c3832267642ef80eb0fe396dc2ca800844fc889f
durga <durgarajesh899@gmail.com>
2022-10-23 14:06:55 +0530

    Adding Mysql script

---
+++ Mysql.sh
@@ -0,0 +1,19 @@
+LOG_FILE=/tmp/mysql
+source common.sh
+
+echo Getting Mysql rep
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>LOG_FILE
+StatusCheck $?
+
+echo Disable Mysql Default  module to enable 5.7 mysql
+dnf module disable mysql -y &>>LOG_FILE
+StatusCheck $?
+
+echo Install Mysql
+yum install mysql-community-server -y  &>>LOG_FILE
+StatusCheck $?
+
+echo Start Mysql service
+systemctl enable mysqld  &>>LOG_FILE
+systemctl restart mysqld  &>>LOG_FILE
+StatusCheck $?
