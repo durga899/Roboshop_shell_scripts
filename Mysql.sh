bc707c092736f3be25c092821412897715ab061a
durga <durgarajesh899@gmail.com>
2022-10-23 14:42:44 +0530

    Adding Mysql script

--- Mysql.sh
+++ Mysql.sh
@@ -18,7 +18,15 @@ systemctl enable mysqld  &>>LOG_FILE
 systemctl restart mysqld  &>>LOG_FILE
 StatusCheck $?
 
+echo Getting default password
 def_paswd=$(grep 'A temporary password'  /var/log/mysqld.log | awk '{print $NF}')
+StatusCheck $?
 
+echo "Sending password changing commands to a file (export mentioned varaible with value in system)"
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${Robo_mysql_paswd}');
      FLUSH PRIVILEGES;" > /tmp/sql_passwd_cmnds
+StatusCheck $?
+
+echo Changing the default mysql root paswd
+mysql -uroot -p"${def_paswd}" < /tmp/sql_passwd_cmnds &>>LOG_FILE
+StatusCheck $?
