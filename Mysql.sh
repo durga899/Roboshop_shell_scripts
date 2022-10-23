5d54d2e4275faab02aaaccb94228424a7d65d2db
durga <durgarajesh899@gmail.com>
2022-10-23 15:07:34 +0530

    Adding Mysql script

--- Mysql.sh
+++ Mysql.sh
@@ -1,22 +1,21 @@
-touch /tmp/mysql
 LOG_FILE=/tmp/mysql
 source common.sh
 
 echo Getting Mysql rep
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>LOG_FILE
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
 StatusCheck $?
 
 echo Disable Mysql Default  module to enable 5.7 mysql
-dnf module disable mysql -y &>>LOG_FILE
+dnf module disable mysql -y &>>$LOG_FILE
 StatusCheck $?
 
 echo Install Mysql
-yum install mysql-community-server -y  &>>LOG_FILE
+yum install mysql-community-server -y  &>>$LOG_FILE
 StatusCheck $?
 
 echo Start Mysql service
-systemctl enable mysqld  &>>LOG_FILE
-systemctl restart mysqld  &>>LOG_FILE
+systemctl enable mysqld  &>>$LOG_FILE
+systemctl restart mysqld  &>>$LOG_FILE
 StatusCheck $?
 
 echo Getting default password
@@ -28,6 +27,9 @@ echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${Robo_mysql_paswd}');
      FLUSH PRIVILEGES;" > /tmp/sql_passwd_cmnds
 StatusCheck $?
 
-echo Changing the default mysql root paswd
-mysql --connect-expired-password -uroot -p"${def_paswd}" </tmp/sql_passwd_cmnds &>>LOG_FILE
-StatusCheck $?
+echo "show databases;" | mysql -uroot -p'${Robo_mysql_paswd}'
+if [ $? -ne 0]; then
+  echo Changing the default mysql root paswd
+  mysql --connect-expired-password -uroot -p"${def_paswd}" </tmp/sql_passwd_cmnds &>>$LOG_FILE
+  StatusCheck $?
+fi
