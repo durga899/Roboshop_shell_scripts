e37124ac1c9d8eb7e315ea655aae990d4ce1b539
durga <durgarajesh899@gmail.com>
2022-10-25 13:13:42 +0530

    Committing changes

--- Frontend.sh
+++ Frontend.sh
@@ -28,7 +28,7 @@ Rolecheck
  mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
 
  echo Updating Roboshop config file
- sed i -e  '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/'   /etc/nginx/default.d/roboshop.conf
+ sed i -e  '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/'   /etc/nginx/default.d/roboshop.conf  &>>${LOG_FILE}
   StatusCheck $?
 
  echo Restarting Nginx
