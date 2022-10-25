90e12f34698e337337c6ddc24308cdcd70263aab
durga <durgarajesh899@gmail.com>
2022-10-25 11:29:36 +0530

    Adding Rabbitmq script

--- Rabbitmq.sh
+++ Rabbitmq.sh
@@ -23,9 +23,12 @@ systemctl enable rabbitmq-server &>>${LOG_FILE}
 systemctl start rabbitmq-server &>>${LOG_FILE}
 StatusCheck $?
 
-echo Adding application user in RabbitMQ
-rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
-StatusCheck $?
+rabbitmqctl list_users | grep roboshop &>>${LOG_FILE}
+if [ $? -ne 0 ]; then
+   echo Adding application user in RabbitMQ
+   rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
+   StatusCheck $?
+fi
 
 echo Adding application user tags in RabbitMQ
 rabbitmqctl set_user_tags roboshop administrator &>>${LOG_FILE}
