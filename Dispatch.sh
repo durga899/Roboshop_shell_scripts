b1d34c2bdf6bc65fa762e98b298002c8a14ef0a0
durga <durgarajesh899@gmail.com>
2022-10-25 12:37:35 +0530

    Adding Rabbitmq script

--- Dispatch.sh
+++ Dispatch.sh
@@ -8,3 +8,15 @@ yum install golang -y &>>${LOG_FILE}
 StatusCheck $?
 
 AddRoboshopUser
+
+echo Dispatching
+go mod init dispatch &>>${LOG_FILE}
+StatusCheck $?
+
+echo Getting
+go get &>>${LOG_FILE}
+StatusCheck $?
+
+echo Build
+go build &>>${LOG_FILE}
+StatusCheck $?
