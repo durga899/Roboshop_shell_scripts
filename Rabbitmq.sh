a5eea2f7bb28f8053cfd18dfd2df428aacc9e25d
durga <durgarajesh899@gmail.com>
2022-10-25 11:24:13 +0530

    Adding Rabbitmq script

--- Rabbitmq.sh
+++ Rabbitmq.sh
@@ -1,37 +1,36 @@
-COMPONENT=rabbitmq
 LOG_FILE=/tmp/rabbitmq
 
 source common.sh
 
 echo Getting Erlang repo
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>>${LOG_FILE}
 StatusCheck $?
 
 echo Installing Erlang
-yum install erlang -y
+yum install erlang -y &>>${LOG_FILE}
 StatusCheck $?
 
 echo Setting YUM repositories for RabbitMQ
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>>${LOG_FILE}
 StatusCheck $?
 
 echo Installing RabbitMQ
-yum install rabbitmq-server -y
+yum install rabbitmq-server -y &>>${LOG_FILE}
 StatusCheck $?
 
 echo Starting RabbitMQ
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl enable rabbitmq-server &>>${LOG_FILE}
+systemctl start rabbitmq-server &>>${LOG_FILE}
 StatusCheck $?
 
 echo Adding application user in RabbitMQ
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
 StatusCheck $?
 
 echo Adding application user tags in RabbitMQ
-rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_user_tags roboshop administrator &>>${LOG_FILE}
 StatusCheck $?
 
 echo Add Permissions for user in RabbitMQ
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG_FILE}
 StatusCheck $?
